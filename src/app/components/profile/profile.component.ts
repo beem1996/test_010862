efc26a81b8323be54c599a73d27f24b1675281a8
beem1996 <varunvarun100939@gmail.com>
2019-01-08 17:43:58 +0700

    แก้ map

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  items: AngularFireList<any>;
+  wikiList: AngularFireList<any>;
   test010362: any[];
   constructor(db: AngularFireDatabase) {
-    this.items = db.list('test010362');
+    this.wikiList = db.list('test010362');
   }
+
   ngOnInit() {
-    this.items.snapshotChanges().map(actions => {
+    this.wikiList.snapshotChanges().map(actions => {
       return actions.map(action => ({ key: action.key, value: action.payload.val() }));
     }).subscribe(items => {
       this.test010362 = items;
